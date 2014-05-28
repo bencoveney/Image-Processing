dba19f404022666dc552194d404f945dd1e691aa
bencoveney <bencoveney@gmail.com>
2014-05-28 01:04:01 +0100

    Defined an interface for Image Transformations

---
+++ Photoshop/Photoshop/BitmapTransform/IBitmapTransform.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Photoshop.BitmapTransform
+{
+    interface IBitmapTransform
+    {
+        /// <summary>
+        /// Launches a dialog box which allows the user to set the transformation's parameters
+        /// </summary>
+        public void ShowParameterDialog();
+
+        /// <summary>
+        /// Creates a new image by applying the Transform to the Source
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public Bitmap Transform(Bitmap Source);
+    }
+}
