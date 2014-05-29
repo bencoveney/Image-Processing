9581e394a0110d649c539823f4bfe8c87e4e8fa0
bencoveney <bencoveney@gmail.com>
2014-05-29 08:46:58 +0100

    Refactored text insertion to BitmapTransform

--- Photoshop/Photoshop/BitmapTransform/TransformInsertText.cs
+++ Photoshop/Photoshop/BitmapTransform/TransformInsertText.cs
@@ -12,8 +12,13 @@ namespace Photoshop.BitmapTransform
     class TransformInsertText : IBitmapTransform
     {
         Form parameterDialog;
-        Bitmap insertImage;
-        int insertX, insertY, insertWidth, insertHeight;
+
+        Bitmap sourceImage;
+
+        string text;
+        Point insertLocation;
+        Font font;
+        Color color;
 
         public void ShowParameterDialog(Bitmap Source)
         {
@@ -22,25 +27,49 @@ namespace Photoshop.BitmapTransform
             parameterDialog.Text = "Insert Image Parameters";
             parameterDialog.Width = 400;
 
-            // Add the image control to the form (with a label)
-            Label imageLabel = new Label();
-            imageLabel.Text = "Image:";
-            imageLabel.Location = new Point(10, 13);
-            parameterDialog.Controls.Add(imageLabel);
-            TextBox imageLocation = new TextBox();
-            imageLocation.Name = "imageLocation";
-            imageLocation.Location = new Point(imageLabel.Width + 20, 10);
-            parameterDialog.Controls.Add(imageLocation);
-            Button imageBrowse = new Button();
-            imageBrowse.Text = "Browse";
-            imageBrowse.Location = new Point(imageLabel.Width + imageLocation.Width + 30, 10);
-            imageBrowse.Click += new EventHandler(BrowseImage);
-            parameterDialog.Controls.Add(imageBrowse);
+            // Add the text input
+            Label textLabel = new Label();
+            textLabel.Text = "Text to Insert:";
+            textLabel.Location = new Point(10, 13);
+            parameterDialog.Controls.Add(textLabel);
+            TextBox textControl = new TextBox();
+            textControl.Name = "textControl";
+            textControl.Location = new Point(textLabel.Right + 10, 10);
+            parameterDialog.Controls.Add(textControl);
+
+            // Add a button to launch the area selection control
+            sourceImage = Source;
+            Button areaSelectionButton = new Button();
+            areaSelectionButton.Text = "Select Insertion Point";
+            areaSelectionButton.Width = 200;
+            areaSelectionButton.Location = new Point(10, textControl.Bottom + 10);
+            areaSelectionButton.Click += new EventHandler(GetSelectedArea);
+            parameterDialog.Controls.Add(areaSelectionButton);
+
+            // Add a button to launch the color selection
+            Button colorSelectionButton = new Button();
+            colorSelectionButton.Text = "Select Color";
+            colorSelectionButton.Name = "colorSelectionButton";
+            colorSelectionButton.Width = 200;
+            colorSelectionButton.Location = new Point(10, areaSelectionButton.Bottom + 10);
+            colorSelectionButton.Click += new EventHandler(GetSelectedColor);
+            parameterDialog.Controls.Add(colorSelectionButton);
+
+            // Add a button to launch the color selection
+            Button fontSelectionButton = new Button();
+            fontSelectionButton.Text = "Select Font";
+            fontSelectionButton.Name = "fontSelectionButton";
+            fontSelectionButton.Width = 200;
+            fontSelectionButton.Height = 50;
+            fontSelectionButton.Location = new Point(10, colorSelectionButton.Bottom + 10);
+            fontSelectionButton.Click += new EventHandler(GetSelectedFont);
+            parameterDialog.Controls.Add(fontSelectionButton);
 
             // Add an apply button to the form
             Button applyButton = new Button();
             applyButton.Text = "Apply";
-            applyButton.Location = new Point(10, imageLocation.Height + 20);
+            applyButton.Width = 200;
+            applyButton.Location = new Point(10, fontSelectionButton.Bottom + 10);
             applyButton.Click += new EventHandler(CloseParameterDialog);
             parameterDialog.Controls.Add(applyButton);
 
@@ -50,38 +79,58 @@ namespace Photoshop.BitmapTransform
 
         private void CloseParameterDialog(object sender, EventArgs e)
         {
-            // Find the image location control
-            TextBox imageLocationControl = parameterDialog.Controls.Find("imageLocation", true).First() as TextBox;
-
-            // Extract the value
-            insertImage = new Bitmap(imageLocationControl.Text);
-
-            // TODO properly calculate dimensions for insert
-            insertX = 0;
-            insertY = 0;
-            insertWidth = insertImage.Width;
-            insertHeight = insertImage.Height;
+            // Retrieve the text
+            TextBox textControl = parameterDialog.Controls.Find("textControl", true).First() as TextBox;
+            text = textControl.Text;
 
             // Close the dialog
             parameterDialog.Close();
         }
 
-        private void BrowseImage(object sender, EventArgs e)
+        /// <summary>
+        /// Launches the area selection to allow the user to select and area to insert to
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GetSelectedArea(object sender, EventArgs e)
         {
-            // Create the file dialog
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.InitialDirectory = @"\C";
-            openFileDialog.FilterIndex = 2;
-            openFileDialog.RestoreDirectory = true;
+            insertLocation = SelectOnImage.SelectPoint(sourceImage);
+        }
 
-            // Show the dialog
-            DialogResult result = openFileDialog.ShowDialog();
+        private void GetSelectedColor(object sender, EventArgs e)
+        {
+            // Show a color selection dialog
+            ColorDialog colorSelector = new ColorDialog();
+            colorSelector.AllowFullOpen = true;
+            colorSelector.AnyColor = true;
+            colorSelector.Color = Color.Red;
+            DialogResult result = colorSelector.ShowDialog();
 
-            // If the dialog was successful open the file
             if (result == DialogResult.OK)
             {
-                TextBox imageLocationControl = parameterDialog.Controls.Find("imageLocation", true).First() as TextBox;
-                imageLocationControl.Text = openFileDialog.FileName;
+                // Retrieve the selected value
+                color = colorSelector.Color;
+
+                // Color the button the selected color
+                Button colorButton = parameterDialog.Controls.Find("colorSelectionButton", true).First() as Button;
+                colorButton.BackColor = color;
+            }
+        }
+
+        private void GetSelectedFont(object sender, EventArgs e)
+        {
+            // Show a color selection dialog
+            FontDialog fontSelector = new FontDialog();
+            DialogResult result = fontSelector.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                // Retrieve the selected value
+                font = fontSelector.Font;
+
+                // Style the button using the selected font
+                Button fontButton = parameterDialog.Controls.Find("fontSelectionButton", true).First() as Button;
+                fontButton.Font = font;
             }
         }
 
@@ -92,9 +141,11 @@ namespace Photoshop.BitmapTransform
             // Convert from Bitmap to Graphics
             Graphics g = Graphics.FromImage(Result);
 
-            // Draw the insert
-            Rectangle rectangle = new Rectangle(insertX, insertY, insertWidth, insertHeight);
-            g.DrawImage(insertImage, rectangle);
+            Brush brush = new SolidBrush(color);
+            Pen pen = new Pen(brush);
+
+            // Draw the string
+            g.DrawString(text, font, brush, insertLocation.X, insertLocation.Y);
 
             // Convert back
             return Result;
